2dbc4c63ab7cf6602c42b069c71668f7f42c89d2
LMellaVillanueva <mellalucas.v@gmail.com>
2026-01-18 17:40:59 -0300

    mejoras en logica

--- mini_api/app/users/controllers/routes.py
+++ mini_api/app/users/controllers/routes.py
@@ -31,18 +31,25 @@ def stats(prop):
         return jsonify({ "error": 'Error en la conexión' }), 500
 
     all_ages = [x['age'] for x in all_users]
+
+    def filter_users(users_list, condition):
+        return [x for x in users_list if condition(x)]
+
     match prop:
         case 'users':
             return jsonify({ "users": all_users }), 200
 
         case 'actives':
-            all_actives = [x for x in all_users if x['active'] and x['age'] >= 18]
-            return jsonify({ "users": all_actives }), 200
+            users = filter_users(all_users, lambda user: user.get('active') is True and user.get('age', 0) >= 18)
+            #! Lógica antigua
+            # all_actives = [x for x in all_users if x['active'] and x['age'] >= 18]
+            return jsonify({ "users": users }), 200
 
         case 'email':
             regex_email = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
-            all_emails = [x for x in all_users if re.fullmatch(regex_email, x['email'])]
-            return jsonify({ "users": all_emails }), 200
+            # all_emails = [x for x in all_users if re.fullmatch(regex_email, x['email'])]
+            users = filter_users(all_users, lambda user: re.fullmatch(regex_email, user.get('email', '')) is not None)
+            return jsonify({ "users": users }), 200
 
         case 'mean':
             val_mean = mean(all_ages) if all_ages else 0
@@ -56,22 +63,26 @@ def stats(prop):
             return jsonify({ "users": user }), 200
         
         case 'all_actives':
-            all_actives = [x for x in all_users if x['active']]
-            return jsonify({ "users": all_actives }), 200
+            # all_actives = [x for x in all_users if x['active']]
+            users = filter_users(all_users, lambda user: user.get('active'))
+            return jsonify({ "users": users }), 200
 
         case 'all_inactives':
-            all_inactives = [x for x in all_users if not x['active']]
-            return jsonify({ "users": all_inactives }), 200
+            # all_inactives = [x for x in all_users if not x['active']]
+            users = filter_users(all_users, lambda user: user.get('active') is False)
+            return jsonify({ "users": users }), 200
 
         case 'max_age':
+            # max_age_users = [x for x in all_users if x['age'] == max_age]
             max_age = max(all_ages)
-            max_age_users = [x for x in all_users if x['age'] == max_age]
-            return jsonify({ "users": max_age_users }), 200
+            users = filter_users(all_users, lambda user: user.get('age') == max_age)
+            return jsonify({ "users": users }), 200
 
         case 'min_age':
+            # min_age_users = [x for x in all_users if x['age'] == min_age]
             min_age = min(all_ages)
-            min_age_users = [x for x in all_users if x['age'] == min_age]
-            return jsonify({ "users": min_age_users }), 200
+            users = filter_users(all_users, lambda user: user.get('age') == min_age)
+            return jsonify({ "users": users }), 200
 
         case _:
             return jsonify({ "error": f"Propiedad '{prop}' no válida" }), 400
@@ -88,6 +99,6 @@ def update_user():
 def delete_user():
     data = request.get_json()
     deleted_user = User.delete_user(data['id'])
-    if not delete_user:
+    if not deleted_user:
         return jsonify({ "error": 'Error en la base de datos' }), 500
     return jsonify({ "deleted_user": True }), 200
